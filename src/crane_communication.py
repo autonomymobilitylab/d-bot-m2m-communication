e284bbb0c9f721177ec79d8a568a2ab9edccaaaf
NkLapp <34657611+NkLapp@users.noreply.github.com>
2023-04-07 00:37:39 +0300

    Ilmatar opcua integration

--- src/crane_communication.py
+++ src/crane_communication.py
@@ -3,18 +3,36 @@ from std_msgs.msg import String
 from ilmatar_python_lib import Crane
 from dotenv import dotenv_values
 
+from d_bot_m2m_communication.srv import Position, PositionResponse
+from d_bot_m2m_communication.srv import Status, StatusResponse
 from resources.config_loader import ConfigLoader
 
 
 class CraneCommunication:
     def __init__(self, opcua_url):
         self.crane = Crane(opcua_url)
+        self.crane_pos_srv = self.start_crane_position_service()
+        self.crane_status_srv = self.start_crane_status_service()
     
-    def get_crane_hook_pos(self):
+    # returns PositionResponse
+    def get_crane_hook_pos(self, req):
         return self.crane.get_coordinates_absolute()
 
+    # return StatusResponse
+    def get_crane_movement_status(self, req):
+        res = True
+        self.crane.get_trolley_speed_request()
+        if (self.crane.get_trolley_speed_request() == 0 and 
+            self.crane.get_bridge_speed_request() == 0 and
+            self.crane.get_hoist_speed_request == 0):
+            res = False
+        return res
 
+    def start_crane_position_service(self):
+        return rospy.Service('position', Position, self.get_crane_hook_pos)
 
+    def start_crane_status_service(self):
+        return rospy.Service('status', Status, self.get_crane_movement_status)
 
 if __name__ == '__main__':
     config = dotenv_values("resources/.env")
@@ -23,4 +41,5 @@ if __name__ == '__main__':
         config.load(['CRANE_OPCUA_URL'])
     crane = CraneCommunication(config['CRANE_OPCUA_URL'])
     rospy.init_node('crane_communication')
+    rospy.spin()
     
