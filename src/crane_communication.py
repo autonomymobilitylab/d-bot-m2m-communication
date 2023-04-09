1df5938204b422845efa7a16ec32beea1bb0fd57
NkLapp <niko.lappi@protonmail.com>
2023-04-09 15:11:44 +0300

    Feature/crane communication ros services (#14)
    * task response for moving tasks between packages
    
    * json messaging conversion start
    
    * fix service response message
    
    ---------
    
    Co-authored-by: Niko Lappi <niko.lappi@aalto.fi>

--- src/crane_communication.py
+++ src/crane_communication.py
@@ -3,11 +3,12 @@ from std_msgs.msg import String
 from ilmatar_python_lib.crane import Crane
 from dotenv import dotenv_values
 
+from api.task import Task
 from d_bot_m2m_communication.srv import Position, PositionResponse
 from d_bot_m2m_communication.srv import Status, StatusResponse
 from d_bot_m2m_communication.srv import CraneTaskCall, CraneTaskCallResponse
+from d_bot_m2m_communication.srv import TaskCall, TaskCallResponse
 from resources.config_loader import ConfigLoader
-
 
 class CraneCommunication:
     def __init__(self, opcua_url):
@@ -21,17 +22,33 @@ class CraneCommunication:
     
     # returns PositionResponse
     def get_crane_hook_pos(self, req):
-        self.crane.connect()
-        res = self.crane.get_coordinates_absolute()
-        self.crane.disconnect()
-        return res
+        task = Task().load(req.task)
+        rospy.loginfo(task.jsonify())
+        try:
+            self.crane.connect()
+            res = self.crane.get_coordinates_absolute()
+            self.crane.connect()
+            if (res):
+                task.location = {
+                    "x": res[0],
+                    "y": res[1],
+                    "z": res[2]
+                }
+            self.crane.disconnect()
+        except:
+            rospy.loginfo('Crane connection failed')
+            task.error = "Crane connection failed"
+        task_json = task.jsonify()
+        response = TaskCallResponse()
+        response.task = task_json
+        return response
 
     # return StatusResponse
     def get_crane_movement_status(self, req):
         self.crane.connect()
         res = True
         self.crane.get_trolley_speed_request()
-        if (self.crane.get_trolley_speed_request() == 0 and 
+        if (self.crane.get_trolley_speed_request() == 0 and
             self.crane.get_bridge_speed_request() == 0 and
             self.crane.get_hoist_speed_request == 0):
             res = False
@@ -45,12 +62,15 @@ class CraneCommunication:
         self.crane.disconnect()
         return True
 
+    # /crane_communication/position
     def start_crane_position_service(self):
-        return rospy.Service('position', Position, self.get_crane_hook_pos)
+        return rospy.Service('/crane_communication/position', TaskCall, self.get_crane_hook_pos)
 
+    # /crane_communication/status
     def start_crane_status_service(self):
         return rospy.Service('status', Status, self.get_crane_movement_status)
     
+    # /crane_communication/stop
     def start_crane_stopper_service(self):
         return rospy.Service('stop', CraneTaskCall, self.stop_crane)
 
